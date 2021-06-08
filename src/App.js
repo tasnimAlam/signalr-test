b081f14553f79620c6703f22c157d63011057abe
Md. Tasnim Alam <tasnimalamcse@gmail.com>
2021-06-08 23:01:35 +0600

    Append message in a list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { HubConnectionBuilder } from "@microsoft/signalr";
 
 function App() {
   const [connection, setConnection] = useState(null);
+  const [exerciseId, setExerciseId] = useState(702);
+  const [messages, setMessages] = useState([]);
+  const messageRef = useRef(null);
 
   useEffect(() => {
     let connection = new HubConnectionBuilder()
@@ -13,19 +16,49 @@ function App() {
     connection.start().then((result) => console.log("Connected"));
   }, []);
 
-  // useEffect(() => {
-  //   if (connection) {
-  //     connection.start().then((result) => {
-  //       console.log("Connected");
-  //     });
-  //   }
-  // }, [connection]);
+  useEffect(() => {
+    if (connection) {
+      connection.on("Update", (message) => {
+        setMessages([...messages, message]);
+
+        const li = document.createElement("li");
+        li.textContent = message;
+
+        messageRef.current.appendChild(li);
+      });
+    }
+  }, [connection]);
+
+  const onStartClick = () => {
+    connection.invoke("Start", exerciseId);
+  };
+
+  const onCancelClick = () => {
+    connection.invoke("Cancel");
+  };
+
+  const onClearMessage = () => {
+    while (messageRef.current.firstChild) {
+      messageRef.current.removeChild(messageRef.current.firstChild);
+    }
+  };
 
   return (
     <div className="App">
       <div>
-        <button>Start</button>
+        <h3>Enter exercise ID</h3>
+        <input
+          value={exerciseId}
+          onChange={(e) => setExerciseId(e.target.value)}
+        />
       </div>
+      <div>
+        <button onClick={() => onStartClick()}>Start</button>
+        <button onClick={() => onCancelClick()}>Cancel</button>
+        <button onClick={() => onClearMessage()}>Clear</button>
+      </div>
+
+      <ul ref={messageRef}></ul>
     </div>
   );
 }
