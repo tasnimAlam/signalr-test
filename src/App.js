c403ce702628e754cb938a3a30c89d3585dc3393
Md. Tasnim Alam <tasnimalamcse@gmail.com>
2021-06-08 21:52:59 +0600

    Add connection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { HubConnectionBuilder } from "@microsoft/signalr";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  const [connection, setConnection] = useState(null);
+
+  useEffect(() => {
+    let connection = new HubConnectionBuilder()
+      .withUrl("http://localhost:5000/hub")
+      .build();
+    setConnection(connection);
+
+    connection.start().then((result) => console.log("Connected"));
+  }, []);
+
+  // useEffect(() => {
+  //   if (connection) {
+  //     connection.start().then((result) => {
+  //       console.log("Connected");
+  //     });
+  //   }
+  // }, [connection]);
+
+  return (
+    <div className="App">
+      <div>
+        <button>Start</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
